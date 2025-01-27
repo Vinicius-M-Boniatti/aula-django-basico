03f75036a1f222d8d1de8a8107cb094dc362b759
02884416056 <02884416056@senacrs.edu.br>
2025-01-27 17:33:28 -0300

    projeto feito em aula

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Pessoa
 
 # Register your models here.
+
+admin.site.register(Pessoa)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- produtos/views.py
+++ produtos/views.py
@@ -1,10 +1,27 @@
+from django import http
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pessoa
 
 # Create your views here.
 
-def ver_produto(request):
-    return render(request, 'ver_produto.html', {'nome': 'vini'})
+def ver_produto(request): 
+    if request.method == 'GET':
+        nome = 'vinicius'   
+        #return render(request, 'ver_produto.html', {'nome': 'vinicius'})
+        return HttpResponse('Deu certo o GET')
+    elif request.method == 'POST': 
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
+        print(nome)
+        print(idade)
+        return HttpResponse(f"Deu certo o POST <br /> Nome: {request.POST.get('nome')}<br /> Idade: {request.POST.get('idade')}")
+        
+
+
+
+
+
 
 
 def inserir_produto(request):
