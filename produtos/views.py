df4d728a2d40505674c07bdfc17f3e9c32daadba
02884416056 <02884416056@senacrs.edu.br>
2025-01-27 15:44:21 -0300

    proje feito em aula

---
+++ produtos/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def ver_produto(request):
+    return render(request, 'ver_produto.html', {'nome': 'vini'})
+
+
+def inserir_produto(request):
+    return render(request, 'inserir_produto.html')
+
+
+
+
+
+
